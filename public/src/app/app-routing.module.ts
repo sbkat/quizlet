039d742243f0016cbb03ecb2ec8ce2f0f25069c3
sbkat <kathydangg@gmail.com>
2019-11-18 11:38:54 -0800

    updated routing

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GamePlatformComponent } from './game-platform/game-platform.component';
+import { CreateQuizComponent } from './create-quiz/create-quiz.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignUpComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'game-platform', component: GamePlatformComponent },
+  { path: 'create-quiz', component: CreateQuizComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/all' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
