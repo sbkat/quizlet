c7a4139d6b83a63d88e7ab8fa68a9b0d8999501e
sbkat <kathydangg@gmail.com>
2019-11-20 10:37:21 -0800

    updated server to fix deprecated warning

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const path = require('path');
 const PORT = 4200;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static( __dirname + '/public/dist/public' ));
 
 require('./server/config/mongoose');
 require('./server/config/routes')(app);
-app.use(express.static( __dirname + '/public/dist/public' ));
+
+app.all('*', (request, response) => {
+    response.sendFile(
+        path.join(__dirname, 'public/dist/public/index.html')
+    );
+});
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,10 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
 
-mongoose.connect('mongodb://localhost/socket_trivia', { 
+mongoose.connect('mongodb://localhost/new_socket', { 
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
     useFindAndModify: false, 
-});
+    }, () => {
+        console.log('We are connected!')
+    })
+        .catch(err => console.log(err));
 
-require('../models/user')
+
+const modelsPath = path.join(__dirname, '../models');
+
+fs.readdirSync(modelsPath).forEach((file) => {
+  if (file.includes('.js')) {
+    require(path.join(modelsPath, file));
+  }
+});
 
